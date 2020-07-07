25f69e0f02e4417c97d709abc6756aac95a45150
raino <rainogong@163.com>
2020-07-07 19:04:12 +0800

    update

--- SmallSoftware/Scanner.cpp
+++ SmallSoftware/Scanner.cpp
@@ -4,15 +4,42 @@
 Scanner::Scanner(char const* buf)
 	: _buf(buf), _iLook(0)
 {
-	std::cout << "Scanner with \"" << buf << "\"" << endl;
+	std::cout << "Scanner with \"" << buf << "\"" << std::endl;
 	Accept();
 }
 
 void Scanner::EatWhite()
 {
 	while (isspace(_buf[_iLook]))
-	{
 		++_iLook;
-	}
 }
 
+EToken Scanner::Accept()
+{
+	EatWhite();
+	switch (_buf[_iLook])
+	{
+	case '+':
+		_token = tPlus;
+		++_iLook;
+		break;
+	case '*':
+		_token = tMult;
+		++_iLook;
+		break;
+	case '0' : case '1' : case '2' : case '3' : case '4' :
+	case '5' : case '6' : case '7' : case '8' : case '9' :
+		_token = tNumber;
+		_number = atoi(&_buf[_iLook]);
+		while(isdigit(_buf[_iLook]))
+			++_iLook;
+		break;
+	case '\0':
+		_token = tEnd;
+		break;
+	default:
+		_token = tError;
+		break;
+	}
+	return Token();
+}
--- SmallSoftware/Scanner.h
+++ SmallSoftware/Scanner.h
@@ -14,13 +14,14 @@ public:
 	Scanner(char const* buf);
 	EToken Token() const { return _token; }
 	EToken Accept();
-	void EatWhite();
 	double Number()
 	{
 		assert(_token == tNumber);
 		return _number;
 	}
 private:
+	void EatWhite();
+
 	char const* const _buf;
 	int	_iLook;
 	EToken	_token;
